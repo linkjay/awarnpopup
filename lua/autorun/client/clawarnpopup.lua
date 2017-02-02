a85c18998a618124b91f31e181b1a3bf1beddcc1
Linkjay <linkjay@linkjay1.com>
2017-02-02 15:14:56 -0800

    Made the script better
    - Added support for super long names
    - Made formatting look a little better

--- lua/autorun/client/clawarnpopup.lua
+++ lua/autorun/client/clawarnpopup.lua
@@ -1,9 +1,11 @@
 AddCSLuaFile()
 
 function link_awarnpopup()
+	local calling_person = net.ReadString()
 	local reason = net.ReadString()
 
 	surface.PlaySound("common/warning.wav")
+	
 	--All of the derma
 	local main = vgui.Create( "DFrame" )
 	main:SetTitle( " " )
@@ -13,9 +15,7 @@ function link_awarnpopup()
 	main:SetDraggable(false)
 	main:MakePopup(true)
 	main.Paint = function( self, w, h )
-
-	draw.RoundedBox( 0, 0, 0, w, h, Color(42, 41, 47, 255) )
-
+		draw.RoundedBox( 0, 0, 0, w, h, Color(42, 41, 47, 255))
 	end
 
 	local main2 = vgui.Create( "DFrame", main )
@@ -26,37 +26,53 @@ function link_awarnpopup()
 	main2:SetDraggable(false)
 	main2:MakePopup(true)
 	main2.Paint = function( self, w, h )
-	draw.RoundedBox( 0, 0, 0, w, h, Color(32, 31, 36, 255) )
+		draw.RoundedBox( 0, 0, 0, w, h, Color(32, 31, 36, 255))
 	end
 
 	local titleText = vgui.Create( "DLabel", main2 )
 	titleText:SetPos( 135.5, 0 )
 	titleText:SetSize( 500, 30)
 	titleText:SetText( "Warning Notification")
-	titleText:SetFont( "Trebuchet24" )
-	titleText:SetTextColor( Color( 204, 0, 0, 255 ) )
+	titleText:SetFont( "Trebuchet24")
+	titleText:SetTextColor( Color( 204, 0, 0, 255 ))
 
-	local titleText2 = vgui.Create( "DLabel", main )
-	titleText2:SetPos( 116.5, 30 )
-	titleText2:SetSize( 500, 30)
-	titleText2:SetText( "You have been issued a warning!")
-	titleText2:SetFont( "HudHintTextLarge" )
-	titleText2:SetTextColor( Color( 204, 0, 0, 255 ) )
+	if string.len(calling_person) >= 16 then
+		local mainMsgPanel = vgui.Create( "DPanel", main )
+		mainMsgPanel:SetPos( -25, 30 )
+		mainMsgPanel:SetSize( 500, 100 )
+		mainMsgPanel.Paint = function()
+			draw.SimpleText( "You have been issued a warning by", "HudHintTextLarge", 500 / 2, 50 / 2, Color( 204, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		end
+
+		local mainMsgPanel2 = vgui.Create( "DPanel", main )
+		mainMsgPanel2:SetPos( -25, 45 )
+		mainMsgPanel2:SetSize( 500, 100 )
+		mainMsgPanel2.Paint = function()
+			draw.SimpleText( calling_person .. "!", "HudHintTextLarge", 500 / 2, 50 / 2, Color( 204, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		end
+	else
+		local mainMsgPanel3 = vgui.Create( "DPanel", main )
+		mainMsgPanel3:SetPos( -25, 30 )
+		mainMsgPanel3:SetSize( 500, 100 )
+		mainMsgPanel3.Paint = function()
+			draw.SimpleText( "You have been issued a warning by " .. calling_person .. "!", "HudHintTextLarge", 500 / 2, 50 / 2, Color( 204, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		end
+	end
 
 	local reasonPanel = vgui.Create( "DPanel", main )
 	reasonPanel:SetPos( -25, 90 )
 	reasonPanel:SetSize( 500, 100 )
 	reasonPanel.Paint = function()
-		draw.SimpleText(reason, "HudHintTextLarge", 500 / 2, 50 / 2, Color( 204, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		draw.SimpleText( reason, "HudHintTextLarge", 500 / 2, 50 / 2, Color( 204, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 	end
 
 	local closeButton = vgui.Create( "DButton", main )
 	closeButton:SetText( "Close" )
-	closeButton:SetTextColor( Color(0, 153, 255) )
+	closeButton:SetTextColor( Color(0, 153, 255))
 	closeButton:SetPos( 252.5, 190 )
 	closeButton:SetSize( 125, 30 )
 	closeButton.Paint = function( self, w, h )
-		draw.RoundedBox( 0, 0, 0, w, h, Color(32, 31, 36, 255) )
+		draw.RoundedBox( 0, 0, 0, w, h, Color(32, 31, 36, 255))
 	end
 	closeButton.DoClick = function()
 		main:Remove()
@@ -64,11 +80,11 @@ function link_awarnpopup()
 
 	local viewWarnsButton = vgui.Create( "DButton", main )
 	viewWarnsButton:SetText( "History" )
-	viewWarnsButton:SetTextColor( Color(0, 153, 255) )
+	viewWarnsButton:SetTextColor( Color(0, 153, 255))
 	viewWarnsButton:SetPos( 72.5, 190 )
 	viewWarnsButton:SetSize( 125, 30 )
 	viewWarnsButton.Paint = function( self, w, h )
-		draw.RoundedBox( 0, 0, 0, w, h, Color(32, 31, 36, 255) )
+		draw.RoundedBox( 0, 0, 0, w, h, Color(32, 31, 36, 255))
 	end
 	viewWarnsButton.DoClick = function()
 		main:Remove()
