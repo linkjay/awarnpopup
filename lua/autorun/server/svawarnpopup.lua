2c2f89e5c4baf5ad058c4f951ead76c9f4401324
Linkjay <linkjay@linkjay1.com>
2017-02-02 15:15:47 -0800

    Made the script better
    - Added support for admin names being displayed.

--- lua/autorun/server/svawarnpopup.lua
+++ lua/autorun/server/svawarnpopup.lua
@@ -2,7 +2,16 @@ util.AddNetworkString( "link_awarnpopup" )
 
 hook.Add("AWarnPlayerWarned", "linkawarndetector", function(target, admin, reason)
 	if IsValid(target) then
+		local calling_person
+
+		if not IsValid(admin) then
+			calling_person = "CONSOLE"
+		else
+			calling_person = admin:GetName()
+		end 
+
 		net.Start( "link_awarnpopup" )
+		net.WriteString(calling_person)
 		net.WriteString(reason)
 		net.Send(target)
 	end
